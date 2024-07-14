2d329a6048ddb715f896c13f553c914bba921742
muralisamui <muralisamui997@gmail.com>
2024-07-14 23:31:57 +0530

    minor fixes

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.enableCors({
-  //   origin: 'http://127.0.0.1:5173',
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  //   allowedHeaders: 'Content-Type, Accept',
-  // });
+  app.enableCors({
+    origin: 'http://127.0.0.1:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  });
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
