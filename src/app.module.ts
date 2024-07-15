adcd86262372b7d51158fedce191c3c2655f25ea
muralisamui <muralisamui997@gmail.com>
2024-07-15 19:30:26 +0530

    chore:url connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,20 +4,30 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CompanyDetailsModule } from './company_details/company_details.module';
 import { CompanyDetails } from './company_details/entity/company_details.entity';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+const DATABASE_URI='postgresql://murali_samui:JkvT0tAK8Yc1I4ss6g45gjBPDL2kdigc@dpg-cqaetnlds78s739qv0q0-a.singapore-postgres.render.com/web_scrapper_db'
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_DBNAME,
-      entities: [CompanyDetails],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env', '.env.dev', '.env.stage', '.env.prod'],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('POSTGRES_URI'),
+        synchronize: false, // Set to true only for development
+        entities: ['dist/**/*.entity.js'],
+        migrations: ['dist/migrations/**/*.js'],
+        subscribers: ['dist/subscriber/**/*.js'],
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }),
     }),
     CompanyDetailsModule,
   ],
