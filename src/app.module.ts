f39dca193e7f70771aa287626dd0f30b8e85c2fe
muralisamui <muralisamui997@gmail.com>
2024-07-15 13:04:37 +0530

    deployment to render

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CompanyDetailsModule } from './company_details/company_details.module';
 import { CompanyDetails } from './company_details/entity/company_details.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '1234',
-      database: 'web_scraper_db',
+      host: process.env.DB_HOST || 'localhost',
+      port: +process.env.DB_PORT || 5432,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_DBNAME,
       entities: [CompanyDetails],
       synchronize: true,
     }),
